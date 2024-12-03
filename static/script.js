14718700d7b8ad2f538da4dac94ea210d156d13b
Otabekkk <otabekiusupov1@gmail.com>
2024-12-03 22:45:43 +0600

    some changes

---
+++ static/script.js
@@ -0,0 +1,20 @@
+const modal = document.getElementById('new_transaction');
+const create_btn = document.getElementById('create_btn');
+
+create_btn.addEventListener('click', () => {
+    modal.style.display = 'flex';
+})
+
+
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+function submitTransaction() {
+    const name = document.getElementById('name').value;
+    const amount = document.getElementById('amount').value;
+    const category = document.getElementById('category').value;
+    
+    alert(`Transaction Created: ${name}, $${amount}, ${category}`);
+    closeModal();
+}
